9d7df76ca4ec919315c3e8bb08677aa2c1d0c8b3
Guido Iaquinti <guido@posthog.com>
2022-01-03 15:24:40 +0100

    WIP

--- ci/kubetest/test_clickhouse_persistence_enabled.py
+++ ci/kubetest/test_clickhouse_persistence_enabled.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 import subprocess
 from typing import Optional
@@ -11,7 +12,7 @@ NAMESPACE="posthog"
 HELM_INSTALL_CMD='''
 helm upgrade \
     --install \
-    -f ../../ci/values/kubetest/test_clickhouse_persistence_true.yaml \
+    -f ../../ci/values/kubetest/test_clickhouse_persistence_enabled.yaml \
     --timeout 30m \
     --create-namespace \
     --namespace posthog \
@@ -21,7 +22,7 @@ helm upgrade \
 '''
 
 @pytest.fixture
-def setup():
+def setup(kube):
     log.debug("🔄 Setting up the k8s cluster...")
     cmd = "kubectl delete all --all -n {namespace}".format(namespace=NAMESPACE)
     cmd_run = subprocess.run(cmd, shell=True)
@@ -38,18 +39,38 @@ def setup():
         pytest.fail("Error while running '{}'. Return code: {}".format(cmd,cmd_return_code))
     log.debug("✅ Done!")
 
-def test_clickhouse_pvc_present(setup, kube):
+    log.debug("🔄 Waiting for all pods to be ready...")
+    start = time.time()
+    timeout = 60
+    while time.time() < start + timeout:
+        pods = kube.get_pods(namespace="posthog")
+        for pod in pods.values():
+            if not pod.is_ready():
+                continue
+        break
+    else:
+        pytest.fail("Timeout raised while waiting for pods to be ready")
+    log.debug("✅ Done!")
+
+def test_volume_claim(setup, kube):
     statefulsets = kube.get_statefulsets(
         namespace="posthog",
         labels={"clickhouse.altinity.com/namespace": "posthog"},
     )
-    volume_claim_templates = list(statefulsets.values())[0].obj.spec.volume_claim_templates
-    assert len(volume_claim_templates) == 1, "ClickHouse should be using a PVC"
+    statefulset_spec = list(statefulsets.values())[0].obj.spec
 
-    volume_claim_template_spec = volume_claim_templates[0].spec
+    volume_claim_template_spec = statefulset_spec.volume_claim_templates[0].spec
+    volume_mounts = statefulset_spec.template.spec.containers[0].volume_mounts
+
+    volume_claim_templates = statefulset_spec.volume_claim_templates
+    assert len(volume_claim_templates) == 1, "ClickHouse should be using a PVC"
 
     volume_claim_template_storage_class = volume_claim_template_spec.storage_class_name
     assert volume_claim_template_storage_class == None, "ClickHouse PVC should be using the default StorageClass"
 
     volume_claim_size = volume_claim_template_spec.resources.requests["storage"]
     assert volume_claim_size == "1Gi", "ClickHouse PVC size should be correct"
+
+    for volume_mount in volume_mounts:
+        if volume_mount.name == "volumeclaim-template-default":
+            assert volume_mount.mount_path == "/var/lib/clickhouse", "ClickHouse PVC mount path should be '/var/lib/clickhouse'"
